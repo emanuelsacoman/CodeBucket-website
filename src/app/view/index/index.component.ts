74e661cb35153f59dbe8c8b69d370413b7879e2c
EvS444 <emanuelsacoman@gmail.com>
2024-03-16 20:31:58 -0300

    Atualizado seleção de itens a serem apresentados no index

--- src/app/view/index/index.component.ts
+++ src/app/view/index/index.component.ts
@@ -28,6 +28,7 @@ export class IndexComponent {
           } as Command;
         });
         this.comandos = this.comandos.filter(item => item.nome !== 'Invite');
+        this.comandos = this.comandos.filter(item => item.nome !== 'Servercount');
         this.comandosLoaded = true;
       });
 
