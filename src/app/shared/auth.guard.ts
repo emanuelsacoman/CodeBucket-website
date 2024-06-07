f2e84a8d44e5f043c9f7fbf0c29fa0c9e707a07c
EvS444 <emanuelsacoman@gmail.com>
2024-06-07 09:14:50 -0300

    Add toast em guarda de rotas

--- src/app/shared/auth.guard.ts
+++ src/app/shared/auth.guard.ts
@@ -7,18 +7,25 @@ import {
 } from '@angular/router';
 import { AuthService } from '../model/services/auth.service';
 import { Observable } from 'rxjs';
+import { NgToastService } from 'ng-angular-popup';
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard {
-  constructor(public authService: AuthService, public router: Router) {}
+  constructor(public authService: AuthService,
+    public router: Router,
+    private toast: NgToastService) {}
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean> | Promise<boolean> | UrlTree | boolean {
     if (this.authService.isLoggedIn !== true) {
-      window.alert('Acesso negado!');
       this.router.navigate(['']);
+      this.toast.error({
+        detail: "Erro",
+        summary: "Acesso Negado!",
+        duration: 5000
+    });
     }
     return true;
   }
